fb15782a9e6f84729613623fcf44f7c7776f4de4
Alexander Zatuliveter <alexander.zatuliveter@gmail.com>
2024-08-04 09:56:33 +0600

    Change player's size and speed.

--- src/player.py
+++ src/player.py
@@ -4,7 +4,7 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()        
-        self.speed = 5
+        self.speed = 3
         base_path = os.path.dirname(__file__)         
         self._body_image = pygame.image.load(os.path.join(base_path, 'sprites/body.png'))
         self._leg_image = pygame.image.load(os.path.join(base_path, 'sprites/leg.png')) 
@@ -32,7 +32,7 @@ class Player(pygame.sprite.Sprite):
         self._blit_leg(self._leg_image, self.leg_rotate)
 
         self.image.blit(self._body_image, (self._x, self._y))
-        
+
         self._blit_leg(self._leg_image, -self.leg_rotate)
         self._blit_arm(self._arm_image, self.arm_rotate)
 
@@ -49,20 +49,20 @@ class Player(pygame.sprite.Sprite):
     def _blit_leg(self, leg_image, angle):
         leg = pygame.transform.rotate(leg_image, angle)
         leg_center = leg.get_rect(center=leg_image.get_rect().center)
-        self.image.blit(leg, (leg_center.x + self._x + 30, leg_center.y + self._y + 240))
+        self.image.blit(leg, (leg_center.x + self._x + 3, leg_center.y + self._y + 24))
 
     def _blit_arm(self, arm_image, angle):
         arm = pygame.transform.rotate(arm_image, angle)
         arm_center = arm.get_rect(center=arm_image.get_rect().center)
-        self.image.blit(arm, (arm_center.x + self._x + 45, arm_center.y + self._y + 10))
+        self.image.blit(arm, (arm_center.x + self._x + 4, arm_center.y + self._y + 1))
 
     def update(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_d] and self._x < 1010:
+        if keys[pygame.K_d] and self._x < 1175:
             self._x += self.speed
         if keys[pygame.K_a] and self._x > 0:
             self._x -= self.speed
-        if keys[pygame.K_s] and self._y < 100:
+        if keys[pygame.K_s] and self._y < 500:
             self._y += self.speed
         if keys[pygame.K_w] and self._y > 0:
             self._y -= self.speed
