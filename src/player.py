364c33666099d85bf873889bb625d77f3fec0d09
Alexander Zatuliveter <alexander.zatuliveter@gmail.com>
2024-08-04 09:40:42 +0600

    Added moved arms

--- src/player.py
+++ src/player.py
@@ -9,29 +9,52 @@ class Player(pygame.sprite.Sprite):
         self._body_image = pygame.image.load(os.path.join(base_path, 'sprites/body.png'))
         self._leg_image = pygame.image.load(os.path.join(base_path, 'sprites/leg.png')) 
         self._leg_image_rect = self._leg_image.get_rect()
+
+        self._arm_image = pygame.image.load(os.path.join(base_path, 'sprites/arm.png')) 
+        self._arm_image_rect = self._arm_image.get_rect()
+
         self._x = x
         self._y = y
+
         self.leg_rotate = 0
+        self.arm_rotate = 0
         self._max_rotate_angle = 25
-        self._leg_rotate_direction = 1
+        self.leg_rotate_direction = 1
+        self.arm_rotate_direction = 1
 
     def _update_image(self):
         self.image = pygame.Surface((self._x + 250, self._y + 900), pygame.SRCALPHA)
         self.rect = self.image.get_rect()
         self._update_legs()
+        self._update_arms()
+
+        self._blit_arm(self._arm_image, -self.arm_rotate)
+        self._blit_leg(self._leg_image, self.leg_rotate)
+
         self.image.blit(self._body_image, (self._x, self._y))
+        
+        self._blit_leg(self._leg_image, -self.leg_rotate)
+        self._blit_arm(self._arm_image, self.arm_rotate)
 
     def _update_legs(self):
         if abs(self.leg_rotate) >= self._max_rotate_angle:
-            self._leg_rotate_direction = -self._leg_rotate_direction
-        self.leg_rotate += self._leg_rotate_direction
-        self._blit_leg(self._leg_image, self.leg_rotate)
-        self._blit_leg(self._leg_image, -self.leg_rotate)
+            self.leg_rotate_direction = -self.leg_rotate_direction
+        self.leg_rotate += self.leg_rotate_direction
+
+    def _update_arms(self):
+        if abs(self.arm_rotate) >= self._max_rotate_angle:
+            self.arm_rotate_direction = -self.arm_rotate_direction
+        self.arm_rotate += self.arm_rotate_direction
     
     def _blit_leg(self, leg_image, angle):
         leg = pygame.transform.rotate(leg_image, angle)
         leg_center = leg.get_rect(center=leg_image.get_rect().center)
         self.image.blit(leg, (leg_center.x + self._x + 30, leg_center.y + self._y + 240))
+
+    def _blit_arm(self, arm_image, angle):
+        arm = pygame.transform.rotate(arm_image, angle)
+        arm_center = arm.get_rect(center=arm_image.get_rect().center)
+        self.image.blit(arm, (arm_center.x + self._x + 45, arm_center.y + self._y + 10))
 
     def update(self):
         keys = pygame.key.get_pressed()
