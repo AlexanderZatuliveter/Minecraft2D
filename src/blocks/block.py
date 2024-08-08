931410bf953f5ee08487d29f84f834bca7298641
Alexander Zatuliveter <alexander.zatuliveter@gmail.com>
2024-08-08 10:01:37 +0600

    performance optimizations.

--- src/blocks/block.py
+++ src/blocks/block.py
@@ -1,9 +1,10 @@
 import pygame
 
-from utils import Utils
+#from utils import Utils
 
 class Block(pygame.sprite.Sprite):
     def __init__(self) -> None:
         super().__init__()
-        utils = Utils()               
-        self.image = utils.load_image('sprites/blocks/empty_block.png')
+        #utils = Utils()
+        self.image = None
+        #self.image = utils.load_image('sprites/blocks/empty_block.png')
--- src/game_field.py
+++ src/game_field.py
@@ -5,7 +5,6 @@ from blocks import Block, Stone
 
 class GameField:
     def __init__(self, x, y):
-        #self.sprites = pygame.sprite.Group()
         self.field = np.zeros((x, y), dtype=object)
         for i in range(x):
             for j in range(y):
@@ -13,19 +12,13 @@ class GameField:
         for n in range(0, 100):
             rand_x = random.randint(1, x-1)
             rand_y = random.randint(1, y-1)
-            print(f"{rand_x=}, {rand_y=}")
             self.field[rand_x][rand_y] = Stone()
 
     def update(self):
-        #for i in np.nditer(self.field, flags=['refs_ok'], op_flags=['readwrite']):
         for (x, y), block in np.ndenumerate(self.field):
-            #if block is Stone: 
             block.update()
         
     def draw(self, screen):
-        #for i in np.nditer(self.field, flags=['refs_ok'], op_flags=['readwrite']):
         for (x, y), block in np.ndenumerate(self.field):
-            #if block is not Stone: continue
-            #block = i.item()
-            #screen.blit(block.image, block.image.get_rect())
-            screen.blit(block.image, (x * 16, y * 16))
+            if block.image:
+                screen.blit(block.image, (x * 24, y * 24))
