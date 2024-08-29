35bcd583fa2dc7f67b82f0cfa6f135063e834a44
Alexander Zatuliveter <alexander.zatuliveter@gmail.com>
2024-08-29 12:37:48 +0600

    spawn random blocks(dirt, stone).

--- src/blocks/dirt.py
+++ src/blocks/dirt.py
@@ -1,6 +1,10 @@
-
+import pygame
 from blocks.block import Block
+from utils import Utils
 
 
 class Dirt(Block):
-    pass
+    def __init__(self) -> None:
+        utils = Utils()
+        self.image = utils.load_image('sprites/blocks/dirt.png')
+        self.is_solid = True
--- src/game_field.py
+++ src/game_field.py
@@ -1,6 +1,6 @@
 import random
 import numpy as np
-from blocks import Stone
+from blocks import Stone, Dirt
 from consts import HALF_SCREEN_HEIGHT, HALF_SCREEN_WIDTH
 
 class GameField:
@@ -9,17 +9,17 @@ class GameField:
         self.field.fill(None)
 
         for i in range(x):  
-            self.field[i][0] = Stone()
-            self.field[i][y-1] = Stone()  
+            self.field[i][0] = random.choice((Dirt(), Stone()))
+            self.field[i][y-1] = random.choice((Dirt(), Stone()))
 
         for j in range(y):
-            self.field[0][j] = Stone()
-            self.field[x-1][j] = Stone()  
+            self.field[0][j] = random.choice((Dirt(), Stone()))
+            self.field[x-1][j] = random.choice((Dirt(), Stone()))
 
         for n in range(100):
             rand_x = random.randint(1, x-1)
             rand_y = random.randint(1, y-1)
-            self.field[rand_x][rand_y] = Stone()
+            self.field[rand_x][rand_y] = random.choice((Dirt(), Stone()))
 
         self.__block_size = 24
 
@@ -33,7 +33,7 @@ class GameField:
                 screen.blit(
                     block.image, 
                     ( 
-                        x * self.__block_size - player.x + HALF_SCREEN_WIDTH, 
+                        x * self.__block_size - player.x + HALF_SCREEN_WIDTH,
                         y * self.__block_size - player.y + HALF_SCREEN_HEIGHT
                     )
                 )
@@ -41,9 +41,9 @@ class GameField:
     def is_solid(self, x, y):
         xpos = int(x / self.__block_size)
         ypos = int(y / self.__block_size)
-        block = self.field[xpos][ypos] 
+        block = self.field[xpos][ypos]
         if block is None: return False
         return block.is_solid
-    
+
     def enumerate(self):
         return [value for index, value in np.ndenumerate(self.field) if value is not None]
