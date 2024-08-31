3aa21e065474c3ea9488963abfd69a496f2343be
Alexander Zatuliveter <alexander.zatuliveter@gmail.com>
2024-08-31 19:45:53 +0600

    fix

--- src/blocks/stone.py
+++ src/blocks/stone.py
@@ -1,10 +1,8 @@
 from blocks.block import Block
-from utils import Utils
 
 
 class Stone(Block):
     def __init__(self) -> None:
         super().__init__()
-        utils = Utils()
-        self.image = utils.load_image('sprites/blocks/stone.png')
+        self._load_image('sprites/blocks/stone.png')
         self.is_solid = True
